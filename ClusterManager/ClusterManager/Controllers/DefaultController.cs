332497b1b81439f92a08246ecdea4cebc1d55a9b
jon <jon.zhao@hdchain.io>
2018-07-25 15:36:18 +0800

    [added]:  Cluster Api Create Genesis Json Config
    1.

---
+++ ClusterManager/ClusterManager/Controllers/DefaultController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using ClusterManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nethereum.KeyStore;
+using Nethereum.Signer;
+using Newtonsoft.Json.Linq;
+
+namespace ClusterManager.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        public static string JsonConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"genesis.json");
+        private static Random Random = new Random();
+        
+
+        [HttpPost]
+        public string CreateGenesis() {
+            var json = System.IO.File.ReadAllText(JsonConfigPath);
+            dynamic jobj = JObject.Parse(json);
+
+            jobj.config.chainId = Random.Next(100000000, 1000000000);
+            jobj.timestamp = $"0x{ GetUnixTime():X}";
+
+            jobj.config.clique.period = 2;
+            
+
+            var accounts = new[] {
+                CreateAccount(),
+                CreateAccount(),
+                CreateAccount(),
+            };
+
+            jobj.extraData =
+                $"0x0000000000000000000000000000000000000000000000000000000000000000{string.Join("",accounts.Select(n=>n.Address.Substring(2)))}0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+            
+            jobj.alloc = new JObject(
+                accounts.Select(n=> 
+                        new JProperty(n.Address,new JObject(
+                            new JProperty("balance","0x200000000000000000000000000000000000000000000000000000000000000")
+                            ))
+                        )
+                );
+
+
+            return jobj.ToString();
+        }
+        
+        private static long GetUnixTime()
+        {
+            return (long) (DateTime.Now - (new DateTime(1970, 1, 1).ToLocalTime())).TotalSeconds;
+
+        }
+
+        private JsonAccount CreateAccount() {
+
+            var key = EthECKey.GenerateKey();
+            var service = new KeyStoreService();
+
+            var pass = Guid.NewGuid().ToString("N").Substring(0,6);
+            var publicAddress = key.GetPublicAddress().ToLower();
+            var json = service.EncryptAndGenerateDefaultKeyStoreAsJson(pass, key.GetPrivateKeyAsBytes(), publicAddress);
+            
+            return new JsonAccount() {
+                Address = publicAddress,
+                Json = json,
+                Password = pass
+            };
+        }
+
+        
+    }
+}
---
+++ ClusterManager/ClusterManager/Models/RequestModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClusterManager.Models
+{
+    public class JsonAccount {
+        public string Address { get; set; }
+
+        public string Json { get; set; }
+
+        public string Password { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
